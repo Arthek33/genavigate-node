a1d7d66214fd4462ba6a9436d89770dc4a5d4982
Schaeffer Mathieu <mathieu@mbpdeschaeffer.home>
2023-11-27 10:47:34 +0100

    first commit

---
+++ models/vehicleModel.js
@@ -0,0 +1,71 @@
+const mongoose = require('mongoose');
+const slugify = require('slugify');
+// const validator = require('validator');
+
+const vehicleSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 40 characters'],
+      // validate: [validator.isAlpha, 'Tour name must only contains characters'],
+    },
+    slug: String,
+    vehicleType: {
+      type: String,
+      required: [true, 'A vehicle must have a type'],
+      enum: {
+        values: ['bike', 'car', 'boat', 'plane'],
+        message: 'Type is either : bike, car, boat or plane',
+      },
+    },
+    price: {
+      type: Number,
+      required: [true, 'A vehicle must have a price'],
+    },
+    speed: {
+      type: Number,
+      required: [true, 'A vehicle must have a speed'],
+    },
+    location: {
+      // GeoJSON
+      type: {
+        type: String,
+        default: 'Point',
+        enum: ['Point'],
+      },
+      coordinates: [Number],
+      address: String,
+      description: String,
+    },
+    imageCover: {
+      type: String,
+      required: [true, 'A vehicle must have a cover image'],
+    },
+    gitf: {
+      type: String,
+      // required: [true, 'A vehicle must have a gitf file'],
+    },
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  },
+);
+
+vehicleSchema.index({ price: 1 });
+vehicleSchema.index({ slug: 1 });
+vehicleSchema.index({ location: '2dsphere' });
+
+// DOCUMENT MIDDLEWARE: runs before .save() and .create()
+vehicleSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+const Vehicle = mongoose.model('Vehicle', vehicleSchema);
+
+module.exports = Vehicle;
---
+++ routes/vehicleRoutes.js
@@ -0,0 +1,43 @@
+const express = require('express');
+const vehicleController = require('../controllers/vehicleController');
+// const authController = require('../controllers/authController');
+
+const router = express.Router();
+
+router
+  .route('/')
+  .get(vehicleController.getAllVehicles)
+  .post(vehicleController.createVehicle);
+
+router
+  .route('/:id')
+  .get(vehicleController.getVehicle)
+  .patch(vehicleController.updateVehicle)
+  .delete(vehicleController.deleteVehicle);
+
+module.exports = router;
+
+// router
+//   .route('/')
+//   .get(tourController.getAllTours)
+//   .post(
+//     authController.protect,
+//     authController.restrictTo('admin', 'lead-guide'),
+//     tourController.createTour,
+//   );
+
+// router
+//   .route('/:id')
+//   .get(tourController.getTour)
+//   .patch(
+//     authController.protect,
+//     authController.restrictTo('admin', 'lead-guide'),
+//     tourController.uploadTourImages,
+//     tourController.resizeTourImages,
+//     tourController.updateTour,
+//   )
+//   .delete(
+//     authController.protect,
+//     authController.restrictTo('admin', 'lead-guide'),
+//     tourController.deleteTour,
+//   );
